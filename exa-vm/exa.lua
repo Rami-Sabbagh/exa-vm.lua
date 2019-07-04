7492e4ea0b6649b96ac3a0306c878174c7c4290c
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2019-07-04 08:39:25 +0300

    Some fields renaming

--- exa-vm/exa.lua
+++ exa-vm/exa.lua
@@ -5,21 +5,21 @@ local exa = class('exa-vm.Exa')
 
 function exa:initialize()
 	self.name = "XA" --EXA name
-	self.code = "" --EXA code
+	self.source = "" --EXA raw code
 
-	self.code_view_mode = 0 --EXA Code windows view mode.
+	self.editor_view_mode = 0 --EXA editor windows view mode.
 	--[0]: Maximized, all code and registers are visible (Default).
 	--[1]: Minimized, no visible code or registers.
 	--[2]: Follow current instruction.
+	--Not much use for a VM, but good for custom editors.
 
 	self.m_register_mode = 0 --EXA M register mode.
 	--[0]: Global (Default).
 	--[1]: Local.
 
 	self.sprite = {} --EXA Sprite data, an array of 100 booleans, from top-left into bottom-right, row by row.
-	for i=1, 100 do
-		self.sprite[i] = false --Initialize the sprite array with empty data.
-	end
+	--Initialize the sprite array with empty data.
+	for i=1, 100 do self.sprite[i] = false end
 end
 
 return exa
