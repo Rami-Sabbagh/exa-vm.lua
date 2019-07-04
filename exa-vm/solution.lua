8d6c062d3d8ad47e866bb8ec9b41bed81d743fc1
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2019-07-04 08:39:15 +0300

    Add the basic save fields

--- exa-vm/solution.lua
+++ exa-vm/solution.lua
@@ -10,7 +10,24 @@ local class = require(root_module..".middleclass")
 local solution = class('exa-vm.Solution')
 
 function solution:initialize()
-	
+	self.version = 1007 --The save file version ?
+
+	self.level_id = "PB039" --The internal EXAPUNKS level ID.
+	--Defaults to the TEC Homebrew (Sandbox) level.
+
+	self.name = "NEW SOLUTION 1" --The solution name.
+	self.competition_wins = 0 --The competition wins count, in battle levels.
+	--completely useless for a VM.
+
+	self.win_statistics = {} --The win statistics, empty when not won, or if non-standard.
+	--Completely useless for a VM.
+	--When the solution is won, this table would have those keys and values:
+	--[cycles]: number, the won cycles count.
+	--[size]: the size of the solution, it's the sum of functional instructions count of all the starting EXAs.
+	--[activity]: the activity count.
+
+	self.starting_exas = {} --The exa instances used when starting the solution execution.
+	--Move instances could be created during execution using the REPL instruction.
 end
 
 return solution
