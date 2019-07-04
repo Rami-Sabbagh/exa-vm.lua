0db981578509438fb770df65d88ee466f834855b
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2019-07-04 09:52:56 +0300

    Wrote EXA data encoder and decoder

--- exa-vm/exa.lua
+++ exa-vm/exa.lua
@@ -1,5 +1,8 @@
-local root_module = ...
+local root_module = string.sub(...,1,-1-string.len("exa"))
+
+--Require some libraries
 local class = require(root_module..".middleclass")
+local utils = require(root_module..".utilities")
 
 local exa = class('exa-vm.Exa')
 
@@ -20,6 +23,34 @@ function exa:initialize()
 	self.sprite = {} --EXA Sprite data, an array of 100 booleans, from top-left into bottom-right, row by row.
 	--Initialize the sprite array with empty data.
 	for i=1, 100 do self.sprite[i] = false end
+end
+
+--Decode the EXA data from an EXAPUNKS .solution save file.
+--The file should be seeked into the EXA "lead-in".
+--file (io file instance): The save file object open in rb mode !
+function exa:decode(file)
+	file:read(1) --Skip the "lead-in"
+	self.name = utils.readString(file)
+	self.source = utils.readString(file)
+	self.editor_view_mode = utils.readInt32(file)
+	self.m_register_mode = utils.readInt32(file)
+	for i=1,100 do
+		self.sprite[i] = utils.readBool(file)
+	end
+end
+
+--Eecode the EXA data into an EXAPUNKS .solution save file.
+--The file should be seeked into the EXA "lead-in".
+--file (io file instance): The save file object open in wb mode !
+function exa:encode(file)
+	file:write("\x0A") --Write the "lead-in"
+	utils.writeString(file,self.name)
+	utils.writeString(file,self.source)
+	utils.writeInt32(file,self.editor_view_mode)
+	utils.writeInt32(file,self.m_register_mode)
+	for i=1,100 do
+		utils.writeBool(file,self.sprite[i])
+	end
 end
 
 return exa
